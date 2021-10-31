c76fc6d43ad9b7c6a6ed9d1581c22304b70ab70f
WaleedAhmed05 <waloo_waleed@yahoo.com>
2021-10-31 16:58:22 -0400

    test commit

--- play-java-starter-example/app/controllers/HomeController.java
+++ play-java-starter-example/app/controllers/HomeController.java
@@ -25,6 +25,7 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
+    //test commit
     public Result index() {
         return ok(
             index.render(
